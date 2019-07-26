f9ef3e6e94fd88166364bf9d7bc10ade7f8c69b5
soler6 <qinnisole@163.com>
2019-07-26 18:00:08 +0800

    first

---
+++ 002type.ts
@@ -0,0 +1,21 @@
+/*
+** type script 之核心 TYPE
+*/
+
+// js 7种基础的type
+// boolean 
+// null
+// undefined
+// number
+// string
+// object
+// symbol (es6)
+
+// Ts 新type
+// 泛型
+// 枚举
+// symbol
+// iterator
+// generator
+
+console.log('hello ts');
